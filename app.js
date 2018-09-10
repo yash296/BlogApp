8868e65f8d14703dc1f8714c9e42c6c5999f9a20
yash296 <wanvari.yash@gmail.com>
2018-09-10 17:06:57 +0530

    Initial setup and Index route added!

---
+++ app.js
@@ -0,0 +1,42 @@
+var	bodyPareser = require("body-parser"),
+ 	mongoose = require("mongoose"),
+ 	express = require("express"),
+ 	app = express();
+//App Config
+ mongoose.connect("mongodb://localhost/blog_app",{ useNewUrlParser: true });
+ app.set("view engine", "ejs");
+ app.use(express.static("public"));
+ app.use(bodyPareser.urlencoded({extended: true}));
+//Moongose model congig
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+	
+//Restful Routes
+
+app.get("/",function(req, res){
+	res.redirect("/blogs");
+	
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("index",{blogs: blogs})
+		}
+	});
+});
+
+
+app.listen(3000, function(req, res){
+	console.log("Blog_App is running on sever 3000");
+});
+		
